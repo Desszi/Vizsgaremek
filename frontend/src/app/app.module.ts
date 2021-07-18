8437ec8b61fdee27223bed6dac7d45332d0aa94f
Mercédesz Anett Porkoláb <09dporkolabm@mail.com>
2021-07-18 20:25:31 +0200

    HttpClientModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient } from  '@angular/common/http';
+import { HttpClientModule } from  '@angular/common/http';
 import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +13,7 @@ import { BaseService } from './service/base.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClient,
+    HttpClientModule,
     FormsModule,
   ],
   providers: [BaseService],
