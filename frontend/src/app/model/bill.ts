d60d64b3440c40143a3d8f88a6a345ad66b4d03b
Mercédesz Anett Porkoláb <09dporkolabm@mail.com>
2021-07-20 20:21:13 +0200

    bills feltöltve

--- frontend/src/app/model/bill.ts
+++ frontend/src/app/model/bill.ts
@@ -5,8 +5,8 @@ export class Bill {
 
     _id: string = "";
     name: string = "";
-    customer: User =  new User();
-    products: Product[] = [];
     grandTotal: number = 0;
     paid: boolean = false;
+    customer: User =  new User();
+    products: Product[] = [];
 }
